6ea33eef47f4ad15f5da064553c026073db79db2
Jose Luis Lucas <joseluis.lucas.simarro@bbva.com>
2018-07-30 11:59:06 +0200

    new tests structure: map[string]struct

--- history/position_test.go
+++ history/position_test.go
@@ -7,37 +7,37 @@ import (
 )
 
 func TestGetLeftNode(t *testing.T) {
-	tests := []struct {
+	tests := map[string]struct {
 		node         position
 		expectedNode position
 	}{
-		{position{0, 1}, position{0, 0}},
-		{position{4, 2}, position{4, 1}},
-		{position{0, 3}, position{0, 2}},
-		{position{8, 3}, position{8, 2}},
+		"Left of (0,1)": {position{0, 1}, position{0, 0}},
+		"Left of (4,2)": {position{4, 2}, position{4, 1}},
+		"Left of (0,3)": {position{0, 3}, position{0, 2}},
+		"Left of (8,3)": {position{8, 3}, position{8, 2}},
 	}
 
-	for _, test := range tests {
+	for testname, test := range tests {
 		leftNode := getLeftNode(test.node)
-		assert.Equalf(t, test.expectedNode, leftNode, "Nodes don't match")
+		assert.Equalf(t, test.expectedNode, leftNode, "Nodes don't match in test: %s", testname)
 	}
 
 }
 
 func TestGetRigthNode(t *testing.T) {
-	tests := []struct {
+	tests := map[string]struct {
 		node         position
 		expectedNode position
 	}{
-		{position{0, 1}, position{1, 0}},
-		{position{4, 2}, position{6, 1}},
-		{position{0, 3}, position{4, 2}},
-		{position{0, 4}, position{8, 3}},
+		"Right of (0,1)": {position{0, 1}, position{1, 0}},
+		"Right of (4,2)": {position{4, 2}, position{6, 1}},
+		"Right of (0,3)": {position{0, 3}, position{4, 2}},
+		"Right of (0,4)": {position{0, 4}, position{8, 3}},
 	}
 
-	for _, test := range tests {
+	for testname, test := range tests {
 		leftNode := getRightNode(test.node)
-		assert.Equalf(t, test.expectedNode, leftNode, "Nodes don't match")
+		assert.Equalf(t, test.expectedNode, leftNode, "Nodes don't match in test: %s", testname)
 	}
 
 }
--- history/tree_test.go
+++ history/tree_test.go
@@ -7,30 +7,31 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// Best practices from https://medium.com/@sebdah/go-best-practices-testing-3448165a0e18
 func TestAdd(t *testing.T) {
 
-	tests := []struct {
+	tests := map[string]struct {
 		event            []byte
 		expectedRootHash hash
 	}{
-		{[]byte(strconv.Itoa((0))), []byte{0xf1, 0x53, 0x43, 0x92, 0x27, 0x9b, 0xdd, 0xbf, 0x9d, 0x43, 0xdd, 0xe8, 0x70, 0x1c, 0xb5, 0xbe, 0x14, 0xb8, 0x2f, 0x76, 0xec, 0x66, 0x7, 0xbf, 0x8d, 0x6a, 0xd5, 0x57, 0xf6, 0xf, 0x30, 0x4e}},
-		{[]byte(strconv.Itoa((1))), []byte{0xfd, 0x9a, 0xf3, 0xd2, 0x89, 0xe1, 0xc7, 0xb2, 0x2c, 0x64, 0x8a, 0x4f, 0x31, 0x63, 0x81, 0xe7, 0xc, 0x63, 0xf8, 0x29, 0x4d, 0x3d, 0xc5, 0x7d, 0xbe, 0xc8, 0x2d, 0x32, 0x92, 0x40, 0x91, 0x5}},
+		"Add 0": {[]byte(strconv.Itoa((0))), []byte{0xf1, 0x53, 0x43, 0x92, 0x27, 0x9b, 0xdd, 0xbf, 0x9d, 0x43, 0xdd, 0xe8, 0x70, 0x1c, 0xb5, 0xbe, 0x14, 0xb8, 0x2f, 0x76, 0xec, 0x66, 0x7, 0xbf, 0x8d, 0x6a, 0xd5, 0x57, 0xf6, 0xf, 0x30, 0x4e}},
+		"Add 1": {[]byte(strconv.Itoa((1))), []byte{0xfd, 0x9a, 0xf3, 0xd2, 0x89, 0xe1, 0xc7, 0xb2, 0x2c, 0x64, 0x8a, 0x4f, 0x31, 0x63, 0x81, 0xe7, 0xc, 0x63, 0xf8, 0x29, 0x4d, 0x3d, 0xc5, 0x7d, 0xbe, 0xc8, 0x2d, 0x32, 0x92, 0x40, 0x91, 0x5}},
 	}
 
 	tree := NewTree()
-	for _, test := range tests {
+	for testname, test := range tests {
 		rootHash := tree.Add(test.event)
-		assert.Equalf(t, test.expectedRootHash, rootHash, "Hashes don't match")
+		assert.Equalf(t, test.expectedRootHash, rootHash, "Hashes don't match in test: %s", testname)
 	}
 
 }
 
 func TestGenerateMembershipProof(t *testing.T) {
-	testsOK := []struct {
+	testsOK := map[string]struct {
 		index, version int
 		expectedTree   historyTree
 	}{
-		{2, 2, historyTree{version: 2,
+		"Index 2 in version 2": {2, 2, historyTree{version: 2,
 			content: map[position]hash{
 				{index: 0, layer: 1}: {},
 				{index: 2, layer: 0}: {},
@@ -38,7 +39,7 @@ func TestGenerateMembershipProof(t *testing.T) {
 			},
 		},
 		},
-		{2, 4, historyTree{version: 4,
+		"Index 2 in version 4": {2, 4, historyTree{version: 4,
 			content: map[position]hash{
 				{index: 0, layer: 1}: {},
 				{index: 2, layer: 0}: {},
@@ -47,11 +48,11 @@ func TestGenerateMembershipProof(t *testing.T) {
 			},
 		},
 		}, // Index > tree version.
-		{100, 2, historyTree{version: -1,
+		"Index greater than version": {100, 2, historyTree{version: -1,
 			content: map[position]hash{},
 		},
 		}, // Version > tree version.
-		{2, 15, historyTree{version: -1,
+		"Version greater than tree version": {2, 15, historyTree{version: -1,
 			content: map[position]hash{},
 		},
 		},
@@ -65,12 +66,12 @@ func TestGenerateMembershipProof(t *testing.T) {
 
 	// Successful test cases.
 	var commitment hash // Unused
-	for _, test := range testsOK {
+	for testname, test := range testsOK {
 		membProof := tree.GenerateMembershipProof(test.index, commitment, test.version)
-		assert.Equalf(t, test.expectedTree.version, membProof.version, "Versions don't match")
-		assert.Equalf(t, len(test.expectedTree.content), len(membProof.content), "Number of nodes don't match")
+		assert.Equalf(t, test.expectedTree.version, membProof.version, "Versions don't match in test: %s", testname)
+		assert.Equalf(t, len(test.expectedTree.content), len(membProof.content), "Number of nodes don't match in test: %s", testname)
 		for node := range membProof.content {
-			assert.Contains(t, test.expectedTree.content, node, "Node not found in expected tree.")
+			assert.Contains(t, test.expectedTree.content, node, "Node not found in expected tree in test: %s.", testname)
 		}
 	}
 
@@ -78,11 +79,11 @@ func TestGenerateMembershipProof(t *testing.T) {
 
 func TestGenerateIncrementalProof(t *testing.T) {
 
-	testsOK := []struct {
+	testsOK := map[string]struct {
 		indexI, indexJ int
 		expectedTree   historyTree
 	}{
-		{2, 3, historyTree{version: 3,
+		"Inc. proof between 2 and 3": {2, 3, historyTree{version: 3,
 			content: map[position]hash{
 				{index: 0, layer: 1}: {},
 				{index: 2, layer: 0}: {},
@@ -90,7 +91,7 @@ func TestGenerateIncrementalProof(t *testing.T) {
 			},
 		},
 		},
-		{2, 4, historyTree{version: 4,
+		"Inc. proof between 2 and 4": {2, 4, historyTree{version: 4,
 			content: map[position]hash{
 				{index: 0, layer: 1}: {},
 				{index: 2, layer: 0}: {},
@@ -101,7 +102,7 @@ func TestGenerateIncrementalProof(t *testing.T) {
 			},
 		},
 		},
-		{3, 7, historyTree{version: 7,
+		"Inc. proof between 3 and 7": {3, 7, historyTree{version: 7,
 			content: map[position]hash{
 				{index: 0, layer: 1}: {},
 				{index: 2, layer: 0}: {},
@@ -111,22 +112,22 @@ func TestGenerateIncrementalProof(t *testing.T) {
 				{index: 7, layer: 0}: {},
 			},
 		},
-		}, // IndexI > IndexJ.
-		{100, 2, historyTree{version: -1,
+		},
+		"Inc. proof when i > j": {100, 2, historyTree{version: -1,
 			content: map[position]hash{},
 		},
-		}, // IndexJ > tree version.
-		{2, 15, historyTree{version: -1,
+		},
+		"Inc. proof when j > tree version": {2, 15, historyTree{version: -1,
 			content: map[position]hash{},
 		},
 		},
 	}
 
-	testsKO := []struct {
+	testsKO := map[string]struct {
 		indexI, indexJ int
 		expectedTree   historyTree
 	}{
-		{2, 4, historyTree{version: 1000,
+		"Wrong inc. proof between 2 and 4": {2, 4, historyTree{version: 1000,
 			content: map[position]hash{
 				{index: 100, layer: 0}: {},
 				{index: 200, layer: 0}: {},
@@ -145,12 +146,12 @@ func TestGenerateIncrementalProof(t *testing.T) {
 	}
 
 	// Successful test cases.
-	for _, test := range testsOK {
+	for testname, test := range testsOK {
 		incrProof := tree.GenerateIncrementalProof(test.indexI, test.indexJ)
-		assert.Equalf(t, test.expectedTree.version, incrProof.version, "Versions don't match")
-		assert.Equalf(t, len(test.expectedTree.content), len(incrProof.content), "Number of nodes don't match")
+		assert.Equalf(t, test.expectedTree.version, incrProof.version, "Versions don't match in test: %s", testname)
+		assert.Equalf(t, len(test.expectedTree.content), len(incrProof.content), "Number of nodes don't match in test: %s", testname)
 		for node := range incrProof.content {
-			assert.Contains(t, test.expectedTree.content, node, "Node not found in expected tree.")
+			assert.Contains(t, test.expectedTree.content, node, "Node not found in expected tree in test: %s", testname)
 		}
 	}
 
