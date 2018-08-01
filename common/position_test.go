855a75f637031cc68e5518f02796cbe3da517b50
Jose Luis Lucas <joseluis.lucas.simarro@bbva.com>
2018-08-01 11:43:03 +0200

    Add travis config

--- common/position_test.go
+++ common/position_test.go
@@ -11,10 +11,10 @@ func TestGetLeftNode(t *testing.T) {
 		node         Position
 		expectedNode Position
 	}{
-		"Left of (0,1)": {Position{0, 1}, Position{0, 0}},
-		"Left of (4,2)": {Position{4, 2}, Position{4, 1}},
-		"Left of (0,3)": {Position{0, 3}, Position{0, 2}},
-		"Left of (8,3)": {Position{8, 3}, Position{8, 2}},
+		"Left of (0,1)": {NewPosition(0, 1), NewPosition(0, 0)},
+		"Left of (4,2)": {NewPosition(4, 2), NewPosition(4, 1)},
+		"Left of (0,3)": {NewPosition(0, 3), NewPosition(0, 2)},
+		"Left of (8,3)": {NewPosition(8, 3), NewPosition(8, 2)},
 	}
 
 	for testname, test := range tests {
@@ -29,10 +29,10 @@ func TestGetRigthNode(t *testing.T) {
 		node         Position
 		expectedNode Position
 	}{
-		"Right of (0,1)": {Position{0, 1}, Position{1, 0}},
-		"Right of (4,2)": {Position{4, 2}, Position{6, 1}},
-		"Right of (0,3)": {Position{0, 3}, Position{4, 2}},
-		"Right of (0,4)": {Position{0, 4}, Position{8, 3}},
+		"Right of (0,1)": {NewPosition(0, 1), NewPosition(1, 0)},
+		"Right of (4,2)": {NewPosition(4, 2), NewPosition(6, 1)},
+		"Right of (0,3)": {NewPosition(0, 3), NewPosition(4, 2)},
+		"Right of (0,4)": {NewPosition(0, 4), NewPosition(8, 3)},
 	}
 
 	for testname, test := range tests {
@@ -40,4 +40,21 @@ func TestGetRigthNode(t *testing.T) {
 		assert.Equalf(t, test.expectedNode, leftNode, "Nodes don't match in test: %s", testname)
 	}
 
+}
+
+func TestComputeNodeValue(t *testing.T) {
+	tests := map[string]struct {
+		node          Position
+		expectedValue float64
+	}{
+		"Value of (0,1)": {NewPosition(0, 1), 1},
+		"Value of (4,2)": {NewPosition(4, 2), 6},
+		"Value of (0,3)": {NewPosition(0, 3), 4},
+		"Value of (0,4)": {NewPosition(0, 4), 8},
+	}
+
+	for testname, test := range tests {
+		value := test.node.ComputeNodeValue()
+		assert.Equalf(t, test.expectedValue, value, "Values don't match in test: %s", testname)
+	}
 }
